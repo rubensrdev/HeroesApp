cca41b34d111926f5b6bf527f04788aa1957757e
Rubén <ruben.sr.apple@gmail.com>
2024-11-04 19:09:31 +0100

    Inicio de proyecto

---
+++ HeroesMasterDetailJSON/HeroesMasterDetailJSON/HeroesMasterDetailJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroesMasterDetailJSONApp.swift
+//  HeroesMasterDetailJSON
+//
+//  Created by Rubén Segura Romo on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HeroesMasterDetailJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
