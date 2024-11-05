82819d5618b8012ed33532bf0e8a4b03aecb11e3
Rubén <ruben.sr.apple@gmail.com>
2024-11-05 19:18:49 +0100

    aplicar fondo gradiente al List de Heroes

--- HeroesMasterDetailJSON/HeroesMasterDetailJSON/Heroes/HeroesList.swift
+++ HeroesMasterDetailJSON/HeroesMasterDetailJSON/Heroes/HeroesList.swift
@@ -13,32 +13,46 @@ struct HeroesList: View {
     var body: some View {
 		
 		NavigationStack {
-			List(heroes) { heroe in
-				HStack {
-					NavigationLink(value: heroe) {
-						Image(heroe.imagen)
-							.resizable()
-							.scaledToFit()
-							.frame(width: 50, height: 50)
-							.clipShape(Circle())
-						VStack(alignment: .leading) {
-							Text(heroe.apodo)
-								.font(.tituloHeroeList)
-								.foregroundStyle(.indigo)
-							Text(heroe.nombreReal)
-								.foregroundStyle(.secondary)
-								.fontDesign(.monospaced)
+			
+			ZStack {
+				LinearGradient(
+					colors: [.verdeGrid, .moradoGrid],
+					startPoint: .topLeading,
+					endPoint: .bottomTrailing
+				)
+				.ignoresSafeArea()
+				
+				List(heroes) { heroe in
+					HStack {
+						NavigationLink(value: heroe) {
+							Image(heroe.imagen)
+								.resizable()
+								.scaledToFit()
+								.frame(width: 50, height: 50)
+								.clipShape(Circle())
+							VStack(alignment: .leading) {
+								Text(heroe.apodo)
+									.font(.tituloHeroeList)
+									.foregroundStyle(.indigo)
+								Text(heroe.nombreReal)
+									.foregroundStyle(.secondary)
+									.fontDesign(.monospaced)
+							}
+							.padding(.leading)
 						}
-						.padding(.leading)
 					}
+					.padding()
+					//.listRowBackground(Color.clear)
 				}
-				.padding()
+				.navigationTitle("Heroes")
+				.navigationDestination(for: Heroe.self) { heroe in
+					HeroeDetalle(heroe: heroe)
+				}
+				.safeAreaPadding()
+				.scrollContentBackground(.hidden)
 			}
-			.navigationTitle("Heroes")
-			.navigationDestination(for: Heroe.self) { heroe in
-				HeroeDetalle(heroe: heroe)
-			}
-			.safeAreaPadding()
+			
+			
 		}
 		
     }
